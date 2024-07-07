6c52341f00c6e27ac323f3c1ce6344379f07cb3b
shiri <shiri7293@gmail.com>
2024-07-07 14:11:39 +0200

    refactor (test): update test descriptions and remove beforeAll

--- tests/cardgame.test.js
+++ tests/cardgame.test.js
@@ -1,53 +1,45 @@
 const {createNewDeck,shuffleDeck,drawACard,playCardGame} = require('../cardgame');
 
-describe('Shuffle deck test', () => {
-    let mathSpy;
-    beforeAll(() => {
-        mathSpy = jest.spyOn(global.Math, 'random').mockReturnValue(0.123456789);
-    });
-
-    afterAll(() => {
-        mathSpy.mockRestore();
-    });
-
-    test('New deck has 40 cards', () => {
+describe('Task 1: Create a shuffled deck of cards test', () => {
+    test('A new deck should contain 40 cards', () => {
         expect(createNewDeck()).toHaveLength(40);
     });
 
-    test('shuffleDeck function shuffles the deck', () => {
+    test('A shuffle function should shuffle a deck', () => {
+        const mathSpy =
+            jest.spyOn(global.Math, 'random')
+                .mockReturnValue(0.123456789); //Mock Math.random()
         const deck = [1,2,3,4,5,6,7,8,9,10];
         const shuffledDeck =  shuffleDeck(deck);
         const expectedShuffledDeck = [9,3,4,5,6,7,8,1,10,2];
 
         expect(shuffledDeck).toStrictEqual(expectedShuffledDeck);
+        mathSpy.mockRestore();
     });
 
 });
 
-describe('Draw card test', () => {
-    let mathSpy;
-    beforeAll(() => {
-        mathSpy = jest.spyOn(global.Math, 'random').mockReturnValue(0.123456789);
-    });
-
-    afterAll(() => {
-        mathSpy.mockRestore();
-    });
-
-    test('If empty draw pile, discard pile is shuffled into draw pile', () => {
-        let player = {
+describe('Task 2: Draw cards test', () => {
+    test('If a player with an empty draw pile tries to draw a card, the discard pile is ' +
+        'shuffled into the draw pile', () => {
+        const mathSpy =
+            jest.spyOn(global.Math, 'random')
+                .mockReturnValue(0.123456789); //Mock Math.random()
+        const player = {
             drawPile: [],
-            discardPile: [1,2,3,4,5,6,7,8,9,10]
+            discardPile: [1,2,3,4,5,6,7,8,9,10],
+            playerName: 'Player1'
         };
         drawACard(player); // To trigger shuffle
         const expectedShuffledDrawPileAfterDrawn = [9,3,4,5,6,7,8,1,10]; // After 2 is drawn from the pile
 
         expect(player.drawPile).toStrictEqual(expectedShuffledDrawPileAfterDrawn);
+        mathSpy.mockRestore();
 
     });
 });
 
-describe('Playing a turn test', () => {
+describe('Task 3: Playing a turn test', () => {
     let consoleSpy;
     beforeEach(() => {
         consoleSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
@@ -57,14 +49,16 @@ describe('Playing a turn test', () => {
         consoleSpy.mockRestore();
     });
 
-   test('When comparing two cards, higher card wins', () => {
+   test('When comparing two cards, the higher card should win', () => {
        let player1 = {
          drawPile: [7],
-         discardPile: []
+         discardPile: [],
+         playerName:'Player 1'
        };
        let player2 = {
          drawPile: [10],
-         discardPile: []
+         discardPile: [],
+         playerName:'Player 2'
        };
 
        playCardGame(player1,player2);
@@ -74,14 +68,16 @@ describe('Playing a turn test', () => {
 
    });
 
-   test('When comparing two cards of same value, winner of next round wins all 4', () => {
+   test('When comparing two cards of the same value, the winner of the next round should win 4 cards', () => {
        let player1 = {
          drawPile: [7,9],
-         discardPile: []
+         discardPile: [],
+         playerName: 'Player1'
        };
        let player2 = {
          drawPile: [7,2],
-         discardPile: []
+         discardPile: [],
+         playerName: 'Player2'
        };
 
        playCardGame(player1,player2);
